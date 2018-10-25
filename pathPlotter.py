65927a2dc361b01b0a3eed2b0ea95c4f98919012
stybl <sty@stybl.net>
2018-10-25 22:52:50 +0300

    Implemented graph mode in plotter

--- pathPlotter.py
+++ pathPlotter.py
@@ -2,26 +2,37 @@ class PathPlotter:
     def __init__(self, img):
         self.img = img
 
+    def get_intermediate_positions(self, start, end):
+        if start[0] == end[0]:
+            step = 1
+            if end[1] - start[1] < 0:
+                step = -1
+            positions = [(start[0],i) for i in range(start[1], end[1], step)]
+        else:
+            step = 1
+            if end[0] - start[0] < 0:
+                step = -1
+            positions = [(i, start[1]) for i in range(start[0], end[0], step)]
+        return positions
+
     def plot(self, path):
         self.img = self.img.convert("RGB")
         pixels = self.img.load()
         for i in range(1, len(path)):
-            current = path[i-1]
-            next = path[i]
-            if current.position[0] == next.position[0]:
-                coefficient = 1
-                if next.position[1] - current.position[1] < 0:
-                    coefficient = -1
-                positions = [(current.position[0], i) for i in range(current.position[1], next.position[1], coefficient)]
-            else:
-                coefficient = 1
-                if next.position[0] - current.position[0] < 0:
-                    coefficient = -1
-                positions = [(i, current.position[1]) for i in range(current.position[0], next.position[0], coefficient)]
-            positions += [current.position,next.position]
+            start = path[i-1].position
+            end = path[i].position
+            positions = self.get_intermediate_positions(start, end) + [start,end]
             for p in positions:
                 pixels[p] = (0,0,255)
-            #pixels[node.position] = (0,0,255)
+
+    def displayGraph(self, graph):
+        self.img = self.img.convert("RGB")
+        pixels = self.img.load()
+        for edge in graph.edges:
+            for pos in self.get_intermediate_positions(edge.start, edge.end):
+                pixels[pos] = (0,0,255)
+        for node in graph.nodes:
+            pixels[node.position] = (255,0,0)
 
     def save(self, filename):
         self.img.save(filename)
